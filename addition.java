5196c888fb9d86cdc22f6fe30a4770e503a1e36c
vamsikrishnaredde <101131285+vamsikrishnaredde@users.noreply.github.com>
2022-03-09 15:43:15 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,11 @@
+package com.example.addition;
+
+public class addition
+{
+	public static void main(String[] args)
+	{
+		int a=20,b=30,c;
+		c=a+b;
+		System.out.println("Addition of two numbers:"+c);
+	}
+}
