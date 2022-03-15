25a3a8f61c5b303eec80e490b4bb27df2de3393a
vamsikrishnaredde <101131285+vamsikrishnaredde@users.noreply.github.com>
2022-03-15 15:47:41 +0530

    Add files via upload

---
+++ variable declaration.js
@@ -0,0 +1,10 @@
+var a=10;
+var b="true";
+var c='t';
+var d= 10.01;
+var e = true;
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
