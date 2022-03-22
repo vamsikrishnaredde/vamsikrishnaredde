7e11e0817d011ebe97e0b4d2e4118b6fb51a08b2
vamsikrishnaredde <101131285+vamsikrishnaredde@users.noreply.github.com>
2022-03-22 16:01:03 +0530

    Add files via upload

---
+++ classdemo07.js
@@ -0,0 +1,10 @@
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+;
+var objectPerson = new Person();
+objectPerson.firstName = "hulk";
+console.log(objectPerson.firstName);
+console.log(objectPerson.lastName);
